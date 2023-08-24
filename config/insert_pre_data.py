7350a1b763996627bd1dccf588d4830d52733ceb
FIXED-TERM Tran Van Nhan (MS/EMN42) <nrh3hc@bosch.com>
2023-08-24 17:06:24 +0700

    Update readme, insert pre data

---
+++ config/insert_pre_data.py
@@ -0,0 +1,17 @@
+from db import conn
+import json
+
+f = open("../config/data.json")
+
+data = json.load(f)
+
+for item in data["shoes"]:
+    conn.storedb.shoes.insert_one(
+        {
+            "image": item["image"],
+            "name": item["name"],
+            "description": item["description"],
+            "price": item["price"],
+            "color": item["color"]
+        }
+    )
--- readme.md
+++ readme.md
@@ -4,5 +4,8 @@
 Using this command: pip install -r requirements.txt
 
 ## 2. Install mongdodb
-## 3. Run the localhost server
+
+## 3. Insert pre data into mongodb
+Run file [config/insert_pre_data.py](config/insert_pre_data.py)
+## 4. Run the localhost server
 Using this command: uvicorn app:app
